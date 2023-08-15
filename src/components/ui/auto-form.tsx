4d29b002877e017de9676c15ddea6d36c1a3ac2b
iamsadat <sadatullahkhan40@gmail.com>
2023-08-15 23:51:52 +0530

    Combobox

--- src/components/ui/auto-form.tsx
+++ src/components/ui/auto-form.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { z } from "zod";
 import {
   Form,
@@ -24,6 +24,19 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Check, ChevronsUpDown } from "lucide-react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "./button";
 import { Input } from "./input";
@@ -460,6 +473,87 @@ function AutoFormEnum({
   );
 }
 
+function AutoFormComboBox({
+  label,
+  isRequired,
+  field,
+  fieldConfigItem,
+  zodItem,
+}: AutoFormInputComponentProps) {
+  const [open, setOpen] = useState(false);
+
+  const baseValues = (getBaseSchema(zodItem) as unknown as z.ZodEnum<any>)._def
+    .values;
+
+  let values: [string, string][] = [];
+  if (!Array.isArray(baseValues)) {
+    values = Object.entries(baseValues);
+  } else {
+    values = baseValues.map((value) => [value, value]);
+  }
+
+  function findItem(value: any) {
+    return values.find((item) => item[0] === value);
+  }
+
+  return (
+    <FormItem>
+      <FormLabel>
+        {label}
+        {isRequired && <span className="text-destructive"> *</span>}
+      </FormLabel>
+      <FormControl>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <div className="flex w-full justify-between">
+              <Button
+                variant="outline"
+                role="combobox"
+                aria-expanded={open}
+                className="w-full justify-between"
+              >
+                {field.value
+                  ? findItem(field.value)?.[1]
+                  : "Select an option..."}
+                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+              </Button>
+            </div>
+          </PopoverTrigger>
+          <PopoverContent className="w-full p-0">
+            <Command>
+              <CommandInput placeholder="Select an option" />
+              <CommandEmpty>No option found.</CommandEmpty>
+              <CommandGroup>
+                {values.map(([value, label]) => (
+                  <CommandItem
+                    key={value}
+                    onSelect={() => {
+                      field.onChange(value);
+                      setOpen(false);
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        field.value === value ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                    {label}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </Command>
+          </PopoverContent>
+        </Popover>
+      </FormControl>
+      {fieldConfigItem.description && (
+        <FormDescription>{fieldConfigItem.description}</FormDescription>
+      )}
+      <FormMessage />
+    </FormItem>
+  );
+}
+
 const INPUT_COMPONENTS = {
   checkbox: AutoFormCheckbox,
   date: AutoFormDate,
@@ -469,6 +563,7 @@ const INPUT_COMPONENTS = {
   textarea: AutoFormTextarea,
   number: AutoFormNumber,
   fallback: AutoFormInput,
+  combobox: AutoFormComboBox,
 };
 
 /**
@@ -480,7 +575,7 @@ const DEFAULT_ZOD_HANDLERS: {
 } = {
   ZodBoolean: "checkbox",
   ZodDate: "date",
-  ZodEnum: "select",
+  ZodEnum: "combobox",
   ZodNativeEnum: "select",
   ZodNumber: "number",
 };
--- src/examples/Api.tsx
+++ src/examples/Api.tsx
@@ -1,4 +1,5 @@
 import * as z from "zod";
+import AutoForm from "../components/ui/auto-form";
 import {
   Card,
   CardContent,
@@ -6,23 +7,20 @@ import {
   CardHeader,
   CardTitle,
 } from "../components/ui/card";
-import { useEffect, useState } from "react";
-import Combobox from "../components/ui/combobox"; // Import the ComboboxDemo component from the separate file
+import React from "react";
 
 function Api() {
-  const [formSchema, setFormSchema] = useState<z.ZodObject<
-    any,
+  const [formSchema, setFormSchema] = React.useState<z.ZodObject<
     any,
     any
   > | null>(null);
-  const [selectedUser, setSelectedUser] = useState<string>("");
 
-  useEffect(() => {
+  React.useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((data) => {
         const formSchema = z.object({
-          user: z.enum(data.map((user: any) => user.name)),
+          Options: z.enum(data.map((user: any) => user.name)),
         });
 
         setFormSchema(formSchema);
@@ -30,30 +28,26 @@ function Api() {
   }, []);
 
   return (
-    <>
-      <div className="mx-auto my-6 max-w-lg">
-        <Card>
-          <CardHeader>
-            <CardTitle>API Example</CardTitle>
-            <CardDescription>
-              The form select options are fetched from an API.
-            </CardDescription>
-          </CardHeader>
+    <div className="mx-auto my-6 max-w-lg">
+      <Card>
+        <CardHeader>
+          <CardTitle>API Example</CardTitle>
+          <CardDescription>
+            The form select options are fetched from an API.
+          </CardDescription>
+        </CardHeader>
 
-          <CardContent>
+        <CardContent>
+          <div className="mb-4">
             {formSchema ? (
-              <Combobox
-                users={formSchema.shape.user._def.values}
-                selectedUser={selectedUser}
-                setSelectedUser={setSelectedUser}
-              />
+              <AutoForm formSchema={formSchema} onSubmit={console.log} />
             ) : (
               <div>Loading...</div>
             )}
-          </CardContent>
-        </Card>
-      </div>
-    </>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
 
